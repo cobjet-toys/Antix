1e33f6df14806e8a3342410d38ce06f577d01d69
stevengill <steve.gill@nitobi.com>
2011-02-20 20:22:39 -0800

    added files to Daemon

---
+++ Daemon/Messages.h
@@ -0,0 +1,17 @@
+#ifndef MESSAGES_H_
+#define MESSAGES_H
+
+//New messages should be added to this header file.//Use int32_t for message members to ensure the same bit size on all machines.
+#include <stdint.h>
+
+//All messages should contain a static const member named 'size'.
+//'size' should specify the byte size of the message, NOT including 'size' itself.
+typedef struct
+{
+    int32_t id;
+    float xpos;
+    float ypos;
+    static const int size = 12;
+} Msg_example;
+
+#endif
