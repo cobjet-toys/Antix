bbd2480362b3372cc7dccf5346309d324d4eaf7b
Ted Tate <eit@kiwi.csil.sfu.ca>
2011-03-01 05:02:13 -0800

    Initial broken-esque version of ClockClient

--- experiments/clock/clock.cpp
+++ experiments/clock/clock.cpp
@@ -7,7 +7,14 @@ int main(int argc, char* argv[])
 
     //Create clock client, create connection with grid servers
     Network::ClockClient c;
-    c.init(argv[1], argv[2]);
+    int i = c.init(argv[1], argv[2]);
+    printf("%i\n", i);
+    if(i < 0)
+    {
+        printf("init failed\n.");
+        return -1;
+    }
+    c.heartbeat();
     printf("waa\n");
     return 0;
 }
