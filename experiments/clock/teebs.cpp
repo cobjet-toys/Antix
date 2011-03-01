a9a61a7aa5e578bab1c26265d53b36d96cf5b039
Tiberiu P <tpa12@sfu.ca>
2011-03-01 01:02:10 -0800

    Oops, actually add the Packer

--- experiments/clock/teebs.cpp
+++ experiments/clock/teebs.cpp
@@ -1,10 +1,17 @@
 #include "ControllerClient.h"
+#include <vector>
 
 int main(int argc, char** argv)
 {
-    using namespace Network;
-    ControllerClient l_CtrlClient = ControllerClient();
-    l_CtrlClient.init(argv[1], argv[2]);
- 
+    typedef std::vector<char[INET6_ADDRSTRLEN]> HostVector;
+    HostVector l_Hosts;
+
+    /*TODO: FILL UP HOSTS FILE WITH GRID SERVER IPS*/
+
+    HostVector::const_iterator l_HostsEnd = l_Hosts.end();  
+    for (HostVector::const_iterator it = l_Hosts.begin(); it != l_HostsEnd; it++)
+    {
+        
+    } 
     return 0;
 }
