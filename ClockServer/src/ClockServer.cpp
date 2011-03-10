498275e9c4e2a70a2005e2c0f27943ead10dbf03
Hardeep <hardeep@HARDEEP-NC10.localdomain>
2011-03-10 02:17:20 -0800

    Yaayy heartbeats

--- ClockServer/src/ClockServer.cpp
+++ ClockServer/src/ClockServer.cpp
@@ -23,6 +23,7 @@ int Network::ClockServer::handler(int fd)
 
 int Network::ClockServer::handleNewConnection(int fd)
 {
+	printf("Adding new Client: %i\n", fd);
 	m_clientMap[fd] = false;
 	m_clientList.push_back(fd);
 	return 0;
@@ -30,37 +31,74 @@ int Network::ClockServer::handleNewConnection(int fd)
 
 int Network::ClockServer::allConnectionReadyHandler()
 {
-	vector<int>::const_iterator l_End = m_clientList.end();
+	printf("All Clients ready\n");
+	
+	std::vector<int>::const_iterator l_End = m_clientList.end();
+	
 	int fd;
 	int i;
+	
 	TcpConnection * conn;
+	
 	Msg_header l_header;
-	Msg_HB heartBeat;
-	unsigned char msg[2];
+	Msg_HB l_heartBeat;
+	
+	unsigned char msg[l_header.size];
 	
 	l_header.sender = SENDER_CLOCK;
 	l_header.message = MSG_HEARTBEAT;
+	l_heartBeat.hb = 1;
 	
-	for(vector<int>::const_iterator it = m_clientList.begin(); it != l_End; it++)
+	printf("Total Clients to send Hb: %i\n",m_clientList.size());
+	
+	for(std::vector<int>::const_iterator it = m_clientList.begin(); it != l_End; it++)
 	{
+		printf("Prepairing heartbeat\n");
 		i = 0;
-		fd = *(it);
-		conn = m_clientMap[fd];
+		fd = (*it);
+		conn = m_Clients[fd];
 		
-		if (conn == NULL) return -1;
+		if (conn == NULL) 
+		{
+			printf("Conn = NULL\n");
+			return -1;
+		}
 		
-		if (pack(&msg, l_header.format, l_header.size) != l_header.size)
+		if (pack(msg, Msg_header_format, l_header.sender, l_header.message) != l_header.size)
 		{
 			return -1; // didn't pack all bytes FAIL & ABORT!!
 		}
 		
-		for (i = 0; i < 100; i++)
+		for (i = 0; i < 10000; i++)
 		{
-			if (conn->send(&msg, l_header.size) == 0) break;
+			if (conn->send(msg, l_header.size) == 0) break;
 		}
 		
-		if (i == 100) return -1; // failed to send msg
+		if (i == 100) 
+		{
+			printf("init failed to send header\n");
+			return -1; // failed to send msg
+		}
 		
+		if (pack(msg, Msg_HB_format, l_heartBeat.hb) != l_heartBeat.size)
+		{
+			return -1; // didn't pack all bytes FAIL & ABORT!!
+		}
+		
+		printf("%i", l_heartBeat.size);
+		
+		for (i = 0; i < 10000; i++)
+		{
+			if (conn->send(msg, l_heartBeat.size) == 0) break;
+		}
+		
+		if (i == 100) 
+		{
+			printf("init failed to send msg\n");
+			return -1; // failed to send msg
+		}
+		
+		printf("Sent heartbeat %hd\n", l_heartBeat.hb);
 	}
 	
 	
