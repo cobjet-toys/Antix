39d11fb93fc11a5cbc1ce90536d8513ee0cb87ba
Curtis Muller <cim1@sfu.ca>
2011-03-12 12:49:56 -0800

    Fixed compile time errors/warnings

--- GridServer/src/GridServer.cpp
+++ GridServer/src/GridServer.cpp
@@ -65,7 +65,7 @@ int GridServer::handler(int fd)
 					unsigned char l_msgSizeBuff;
 					
                     
-					if (l_curConnection->recv(, msgSize.size))
+					if (l_curConnection->recv(msg, l_Req.size))
 					{
 						DEBUGPRINT("Request for robots failed\n");
 						break;
@@ -126,6 +126,8 @@ int GridServer::handler(int fd)
 			{
 		    		
 			    case MSG_ASSIGN_ID:
+				{
+					Msg_header l_msgHeader = {SENDER_GRIDSERVER, MSG_ASSIGN_ID};
 				    Msg_uId l_uId;
 					memset(msg, 0, l_uId.size);
 							
@@ -150,7 +152,7 @@ int GridServer::handler(int fd)
 							
 					return 0;
 					break;
-						
+				}		
 			    default:
 					printf("ERROR: no matching msg handler for CONTROLLER\n");
 					return 0; // no event handler for this call
@@ -191,7 +193,7 @@ int GridServer::handler(int fd)
 			break;
 		
 	}
-	delete msg;
+	//delete msg;	//Not requried, using heap memeory, the program will automatically clean it up when it ends. Unless you use 'new', you should not call delete.
     return 0;
 }
 
