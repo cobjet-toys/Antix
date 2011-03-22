6db6d249dcc6e11272b1de6f9ba5a658c177493e
Curtis Muller <cim1@sfu.ca>
2011-03-22 00:20:05 -0700

    Fixed merge conflect agains stub function COMMAND==GRID&&commands.size()>=4

--- RobotClient/src/RobotParser.cpp
+++ RobotClient/src/RobotParser.cpp
@@ -26,16 +26,11 @@ int RobotParser::handler(std::vector<std::string> commands, void *args)
 		std::string command = commands.at(0);
 		if ( command == "GRID" && commands.size() >= 4)
 		{
-<<<<<<< HEAD
-			printf("PARSED: GRID with IP %s and PORT %s\n", commands.at(1).c_str(), commands.at(2).c_str(), grid_id);
-			return l_robot->initGrid(commands.at(1).c_str(), commands.at(2).c_str(), grid_id);
-=======
 			int id = atoi(commands.at(1).c_str());
 			const char * host = commands.at(2).c_str();
 			const char * port = commands.at(3).c_str();
 			printf("PARSED: GRID ID %i with IP %s and PORT %s\n", id, host, port);
 			return l_robot->initGrid(host,port, id);
->>>>>>> 61e41acf239cc670f3a90b2da1ca056f9e8003fb
 		}
 		if ( command == "CLOCK" && commands.size() >= 3)
 		{	
