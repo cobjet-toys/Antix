8c3a2e5997ac68c3701254cbf018d347ee459d01
Leif Stroman <leifstroman@hotmail.com>
2011-03-26 13:29:04 -0700

    - Fixed translation while in zoom. - Added optimized function for drawing robots circles (only a test func atm)

--- Drawer/includes/Gui.h
+++ Drawer/includes/Gui.h
@@ -2,6 +2,12 @@
 
 #include <math.h>
 
+#ifdef __linux
+#include <GL/glut.h>
+#elif __APPLE__
+#include <GLUT/glut.h>
+#endif
+
 void * listener_function(void* args);
 void initGraphics(int argc, char **argv);
 void idleFunc();
@@ -11,12 +17,14 @@ void drawPucks();
 void drawTeams();
 void drawRobots();
 void drawText(char* text, float x, float y);
-void drawTest();
+void drawTest(int robotCount, int edgePoints);
 //void drawRobot(Game::Robot* robot, const GUI::Color* color);
 //void drawHome(Game::Home* home, const GUI::Color* color);
 void mousFunc(int button, int state, int x, int y);
 void GlDrawCircle( double x, double y, double r, double count );
 static int drawCount = 0;
+
+// --- Required Graphics Variables --- //
 
 // Values for mouse drag/zoom options
 static int actionType = 0;
@@ -28,3 +36,7 @@ static float zoomLevel = 1.0;
 
 // Viewport rect positioning values
 static int left = 0, top = 0, right = 0, bottom = 0;
+
+// Lookup arrays for position values based on angle
+static GLfloat xVals[360];
+static GLfloat yVals[360];
