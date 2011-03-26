4bd293444b6bc8eed4e81431693c4a576a347fdc
Leif Stroman <leifstroman@hotmail.com>
2011-03-25 23:06:56 -0700

    - Added translation and zoom to drawer (left click & drag to translate, right click & drag to zoom)

--- Drawer/includes/Gui.h
+++ Drawer/includes/Gui.h
@@ -17,3 +17,14 @@ void drawTest();
 void mousFunc(int button, int state, int x, int y);
 void GlDrawCircle( double x, double y, double r, double count );
 static int drawCount = 0;
+
+// Values for mouse drag/zoom options
+static int actionType = 0;
+
+static bool inDrag = false;
+static int xCur = -1, yCur = -1, xInit = -1, yInit = -1, xEnd = -1, yEnd = -1;
+
+static float zoomLevel = 1.0;
+
+// Viewport rect positioning values
+static int left = 0, top = 0, right = 0, bottom = 0;
