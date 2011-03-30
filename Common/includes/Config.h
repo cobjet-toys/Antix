1b84513c8c5622e01f1f78c16af59a569d401cc7
Hardeep Shoker <shokerhardeep@hotmail.com>
2011-03-30 15:45:00 -0700

    log print

--- Common/includes/Config.h
+++ Common/includes/Config.h
@@ -1,13 +1,20 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define DEBUG true
 #define RELEASE
+#define LOGGING
 
 #ifdef RELEASE  
 #define DEBUGPRINT(format, args...) ((void)0)
 #else
 #define DEBUGPRINT printf
 #endif
+
+#ifdef LOGGING  
+#define LOGPRINT(format, args...) ((void)0)
+#else
+#define LOGPRINT printf
+#endif
+
 
 #endif
